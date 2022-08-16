750094768c79b08001ff2c107a3e70e60cdcdf2a
deyki <deikioveca@gmail.com>
2022-08-16 16:53:59 +0300

    "UserController" class added

---
+++ src/main/java/deyki/EBank/controller/UserController.java
@@ -0,0 +1,34 @@
+package deyki.EBank.controller;
+
+import deyki.EBank.domain.model.bindingModel.user.NewUsernameModel;
+import deyki.EBank.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+
+        return ResponseEntity.status(HttpStatus.OK).body("Hello!");
+    }
+
+    @PutMapping("/changeUsername/{userId}")
+    public ResponseEntity<String> changeUsername(@PathVariable Long userId, @RequestBody NewUsernameModel newUsernameModel) {
+
+        userService.changeUsernameById(userId, newUsernameModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Username changed successfully!");
+    }
+}
