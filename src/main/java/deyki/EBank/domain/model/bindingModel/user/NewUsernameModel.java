ecc61f991c29d65606590d6dc567c77b51bf3d3c
deyki <deikioveca@gmail.com>
2022-08-16 16:54:28 +0300

    Binding model for new username creation

---
+++ src/main/java/deyki/EBank/domain/model/bindingModel/user/NewUsernameModel.java
@@ -0,0 +1,15 @@
+package deyki.EBank.domain.model.bindingModel.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class NewUsernameModel {
+
+    private String username;
+}
